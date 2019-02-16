5ea30ac8a0fa4273c1a3df807b975703feabad32
Cauê Baasch de Souza <cauebs@pm.me>
2019-02-16 01:29:51 -0200

    Change average_length to work on a per-chat basis.

--- karmagrambot/analytics.py
+++ karmagrambot/analytics.py
@@ -3,9 +3,9 @@ import dataset
 from .config import DB_URI
 
 
-def average_message_length(user_id):
+def average_message_length(user_id, chat_id):
     db = dataset.connect(DB_URI)
-    messages = db['messages'].find(user_id=user_id)
-    messages = list(messages)  # because otherwise results are read lazily
+    messages = db['messages'].find(user_id=user_id, chat_id=chat_id)
+    messages = [m for m in messages if m['length'] is not None]
 
     return sum(m['length'] for m in messages) / len(messages)
--- karmagrambot/commands.py
+++ karmagrambot/commands.py
@@ -6,9 +6,10 @@ from .analytics import average_message_length
 def average_length(bot, update):
     message = update.message
     user_id = message.from_user.id
+    chat_id = message.chat.id
 
-    response = f'{average_message_length(user_id):.3}'
-    message.reply_text(response)
+    average = average_message_length(user_id, chat_id)
+    message.reply_text(f'{average:.3}')
 
 
 HANDLERS = [
