75417d5e965b274e890664e7f244644011f7b6bf
Djalma <djalmirinha@gmail.com>
2019-06-08 17:25:20 -0300

    Correcting spacing and documentation

--- karmagrambot/analytics.py
+++ karmagrambot/analytics.py
@@ -13,18 +13,28 @@ def average_message_length(user_id, chat_id):
 
     return sum(m['length'] for m in messages) / len(messages)
 
-def get_karma(user_id, chat_id):
-	db = dataset.connect(DB_URI)
+def get_karma(user_id: int, chat_id: int) -> int:
+    """Get the karma of an given user in a given chat.
+
+    Args:
+        user_id: The id of the user that we want to get the karma.
+        chat_id: The id of the chat we're interested in.
+
+    Returns:
+        The karma value.
+    """
+
+    db = dataset.connect(DB_URI)
     user_messages = db['messages'].find(user_id=user_id, chat_id=chat_id)
 
     good_karma = 0
     bad_karma = 0
     for message in user_messages:
-    	replies_to_message = db['messages'].find(replied=message['message_id'])
-    	for reply in replies_to_message:
-    		if reply['vote'] == '+':
-    			good_karma += 1
-    		elif reply['vote'] == '-':
-    			bad_karma += 1
+        replies_to_message = db['messages'].find(replied=message['message_id'])
+        for reply in replies_to_message:
+            if reply['vote'] == '+':
+                good_karma += 1
+            elif reply['vote'] == '-':
+                bad_karma += 1
 
-	return good_karma - bad_karma
+    return good_karma - bad_karma
--- karmagrambot/commands.py
+++ karmagrambot/commands.py
@@ -4,15 +4,15 @@ from . import analytics
 
 
 def average_length(bot: telegram.Bot, update: telegram.Update):
-	"""Get the average message length of the user who invoked the command.
+    """Reply the user who invoked the command with hers/his average message length.
 
-	Args:
-		bot: the object that represents the Telegram Bot.
-		update: the object that represents an incoming update for the bot to handle.
+    Args:
+        bot: the object that represents the Telegram Bot.
+        update: the object that represents an incoming update for the bot to handle.
 
-	Returns:
-		Doesn't return anything, but reply the user with hers/his respective average length.
-	"""
+    Returns:
+        Doesn't return anything, but reply the user with hers/his respective average length.
+    """
 
     average = analytics.average_message_length(update.message.from_user.id, update.message.chat.id)
     response = f'{average:.3}'
@@ -20,19 +20,19 @@ def average_length(bot: telegram.Bot, update: telegram.Update):
     update.message.reply_text(response)
 
 def karma(bot: telegram.Bot, update: telegram.Update):
-	"""Get the karma of the user who invoked the command.
+    """Reply the user who invoked the command with hers/his respective karma.
 
-	Args:
-		bot: the object that represents the Telegram Bot.
-		update: the object that represents an incoming update for the bot to handle.
+    Args:
+        bot: the object that represents the Telegram Bot.
+        update: the object that represents an incoming update for the bot to handle.
 
-	Returns:
-		Doesn't actually return anything, but answer the user with hers/his respective karma.
-	"""
+    Returns:
+        Doesn't actually return anything, but answer the user with hers/his respective karma.
+    """
 
-	karma = analytics.get_karma(update.message.from_user.id, update.message.chat.id)
+    karma = analytics.get_karma(update.message.from_user.id, update.message.chat.id)
 
-	update.message.reply_text(karma)
+    update.message.reply_text(karma)
 
 HANDLERS = [
     CommandHandler('average_length', average_length),
