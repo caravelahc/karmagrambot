690e2d32af99e4990561b885f196cdfa4198844f
Djalma <djalmirinha@gmail.com>
2019-06-11 10:01:36 -0300

    Adding karmas command

--- karmagrambot/commands.py
+++ karmagrambot/commands.py
@@ -36,7 +36,27 @@ def karma(bot: Bot, update: Update):
 
     update.message.reply_text(karma)
 
+def karmas(bot: Bot, update: Update):
+    """Shows the top 10 karmas in a given group, if the groups doesn't have at least 10 users, show the maximum amount
+
+    Args:
+        bot: The object that represents the Telegram Bot.
+        update: The object that represents an incoming update for the bot to handle.
+
+    Returns:
+        Doesn't return anything, but answer the user with the top 10 karmas in the group.
+    """
+
+    top_users = analytics.get_top_10_karmas(update.message.chat.id)
+
+    response = ''
+    for i in range(len(top_users)):
+        response += '{} - {} ({})\n'.format(i + 1, top_users[i]['name'], top_users[i]['karma'])
+
+    update.message.reply_text(response)
+
 HANDLERS = [
     CommandHandler('average_length', average_length),
     CommandHandler('karma', karma),
+    CommandHandler('karmas', karmas),
 ]
