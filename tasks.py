6dac275c6b38b34479a2e7c9265ca1cdba57844d
Tarcisio Eduardo Moreira Crocomo <tarcisio.crocomo@gmail.com>
2019-06-13 00:10:48 -0300

    Add invoke and basic tasks.

---
+++ tasks.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from invoke import Result, UnexpectedExit, task
+
+
+def check_all(results: List[Result]):
+    try:
+        result = next(result for result in results if result.exited != 0)
+    except StopIteration:
+        pass
+    else:
+        raise UnexpectedExit(result)
+
+
+@task
+def format(c):
+    c.run('yapf -ri karmagrambot')
+    c.run('isort -rc -y -q karmagrambot')
+
+
+@task
+def format_check(c):
+    check_all([
+        c.run('yapf -rd karmagrambot', warn=True),
+        c.run('isort -rc -c -q karmagrambot', warn=True),
+    ])
