3974e66e8c3005aefd3815d3d6c89b154dc33837
Djalma <djalmirinha@gmail.com>
2019-08-20 00:17:44 -0300

    Reorganizing some things I messed up (hopefully, the final branch commit)

--- karmagrambot/analytics.py
+++ karmagrambot/analytics.py
@@ -1,10 +1,20 @@
 import dataset
 
-from .config import DB_URI
+from typing import List, Dict
 
+from .config import DB_URI
 from .types import UserKarma, DevilSaint
 
-def average_message_length(user_id, chat_id):
+def average_message_length(user_id: int, chat_id: int) -> float:
+    """Get the average message length of an given user in a given chat
+
+    Args:
+        user_id: The id of the user that we want the average.
+        chat_id: THe id of the chat we're interested in.
+    Returns:
+        The average.
+    """
+
     db = dataset.connect(DB_URI)
     messages = db['messages'].find(user_id=user_id, chat_id=chat_id)
     messages = [m for m in messages if m['length'] is not None]
@@ -40,7 +50,36 @@ def get_karma(user_id: int, chat_id: int) -> int:
 
     return good_karma - bad_karma
 
-def get_devil_saint(chat_id: int) -> dict:
+
+def get_top_n_karmas(chat_id: int, n: int) -> List[UserKarma]:
+    """Get the top n karmas in a given group, if the doesn't have enough users, return the total amount.
+    Args:
+        chat_id: The id of the chat that we're interested in.
+        n: The number of desired karmas
+    Returns:
+        A list with top n users with better karma.
+    """
+    db = dataset.connect(DB_URI)
+    users = db['tracked'].find(chat_id=chat_id)
+    user_ids = [u['user_id'] for u in users]
+    
+    sorted_user_ids = sorted(user_ids, key=lambda u: get_karma(u, chat_id), reverse=True)
+
+    sorted_users = []
+    for user_id in sorted_user_ids:
+        user = db['users'].find(user_id=user_id)
+
+        name = user_name(*user)
+
+        user = UserKarma(name, get_karma(user_id, chat_id))
+
+        sorted_users.append(user)
+
+    top_n = sorted_users[:n]
+
+    return top_n
+
+def get_devil_saint(chat_id: int) -> DevilSaint:
     """Get the users with the lowest and the greatest karma in a given chat.
 
     Args:
@@ -57,9 +96,17 @@ def get_devil_saint(chat_id: int) -> dict:
     devil_id = sorted(user_ids, key=lambda u: get_karma(u, chat_id))[0]
     saint_id = user_ids[-1]
 
-    devil = db['users'].find_one(user_id=devil_id)
-    saint = db['users'].find_one(user_id=saint_id)
+    devil = db['users'].find(user_id=devil_id)
+    saint = db['users'].find(user_id=saint_id)
 
     devil_saint = DevilSaint(UserKarma(user_name(*devil), get_karma(devil_id, chat_id)), UserKarma(user_name(*saint), get_karma(saint_id, chat_id)))
 
     return devil_saint
+
+def user_name(user: Dict[str, str]) -> str:
+    name = user['first_name']
+
+    if user['last_name'] is not None:
+        name += f" {user['last_name']}"
+    
+    return name
